949b2a936d9030cabfb0bfb38ab2446a24507239
Chris Lewis <13chlewis@gmail.com>
2018-11-28 16:52:57 -0700

    Round Class

---
+++ lib/round.rb
@@ -0,0 +1,67 @@
+require 'pry'
+require './lib/card'
+require './lib/turn'
+require './lib/deck'
+
+class Round
+  attr_reader :turns
+  def initialize(deck)
+  @deck = deck
+  @turns = []
+  end
+
+  def current_card
+  @deck.cards[0]
+  end
+
+  def take_turn(guess)
+  new_turn = Turn.new(guess, current_card)
+  @turns << new_turn
+  @deck.cards.shift
+  return new_turn
+  end
+
+  def number_correct
+    correct = 0
+  @turns.each do |turn|
+    if turn.correct?
+      correct += 1
+      return correct
+      end
+    end
+  end
+
+  def number_correct_by_category(category)
+    correct = 0
+  @turns.each do |turn|
+    if turn.card.category == category
+    if turn.correct?
+      correct += 1
+    end
+    end
+  end
+    return correct
+  end
+
+  def percent_correct
+    correct = 0.0
+    total = @turns.count
+    @turns.each do |turn|
+      if turn.correct?
+        correct += 1
+      end
+    end
+    percent = (correct / total) * 100.0
+    return percent
+  end
+end
+
+
+card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+cards = [card_1, card_2, card_3]
+deck = Deck.new(cards)
+round = Round.new(deck)
+binding.pry
