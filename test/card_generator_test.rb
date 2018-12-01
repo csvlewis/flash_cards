939828853ece08835db6840c3e9a91563164b8a8
Chris Lewis <13chlewis@gmail.com>
2018-12-01 15:28:25 -0700

    Generator test complete

---
+++ test/card_generator_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require './lib/card_generator'
+require './lib/deck'
+require './lib/round'
+
+class CardGeneratorTest < Minitest::Test
+
+  def test_it_exists
+    cards = CardGenerator.new('cards.txt')
+
+    assert_instance_of CardGenerator, cards
+  end
+
+  def test_it_creates_a_deck
+    cards = CardGenerator.new('cards.txt')
+    deck = Deck.new(cards.cards)
+
+    assert_instance_of Deck, deck
+  end
+
+  def test_card_created_from_text_file
+    cards = CardGenerator.new('cards.txt')
+    deck = Deck.new(cards.cards)
+
+    assert_instance_of Card, deck.cards[0]
+    assert_equal "central perk", deck.cards[1].answer
+  end
+
+  def test_it_creates_a_round
+    cards = CardGenerator.new('cards.txt')
+    deck = Deck.new(cards.cards)
+    round = Round.new(deck)
+
+    assert_instance_of Round, round
+  end
+end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,5 +1,4 @@
 require 'minitest/autorun'
-require 'minitest/pride'
 require './lib/deck'
 require './lib/card'
 
